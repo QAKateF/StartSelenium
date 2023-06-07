8c64a681db1efed9da4a7ec469dead2ee2c17bc5
QAKateF <134771804+QAKateF@users.noreply.github.com>
2023-06-07 20:17:19 +0300

    Update TableTest.java

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -1,18 +1,29 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class TableTest {
     WebDriver wd;
 
-    @BeforeTest
-    public void precondition(){
+    @BeforeMethod
+    public void init(){
         wd = new ChromeDriver();
-        wd.get("https://telranedu.web.app/home");
+        wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
     }
 
+    @Test
+    public void testCSS() {
+        wd.findElement(By.cssSelector("#customers tr:nth-child(7) td:last-child"));
+    }
 
-
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
 
 
 }

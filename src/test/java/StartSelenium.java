24b20024b2ac1f742428f1d7bcb0d6a5b324ba69
QAKateF <134771804+QAKateF@users.noreply.github.com>
2023-06-14 18:22:09 +0300

    Update StartSelenium.java

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -20,19 +20,31 @@ public class StartSelenium {
         //old strategies
         wd.findElements(By.tagName("a"));
         wd.findElements(By.cssSelector("a"));
+        wd.findElements(By.xpath("//a"));
 
         wd.findElements(By.id("root"));
         wd.findElements(By.cssSelector("#root"));
+        wd.findElements(By.xpath("//*[@id='root']"));
 
         wd.findElement(By.className("container"));
         wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.xpath("//*[@class='container']"));
 
         wd.findElement(By.linkText("HOME"));
+        wd.findElement(By.xpath("//a[text()='HOME']"));
+        wd.findElement(By.xpath("//a[.='HOME']"));
+
         wd.findElement(By.partialLinkText("OM"));
 
         wd.findElement(By.cssSelector("[href='/login']"));
+        wd.findElement(By.xpath("//a[@href='/login']"));
+
         wd.findElement(By.cssSelector("[href*='og']"));
+        wd.findElement(By.xpath("//a[contains(@href,'og')]"));
+
         wd.findElement(By.cssSelector("[href^='/log']"));
+        wd.findElement(By.xpath("//a[starts-with(@href,'/log')]"));
+
         wd.findElement(By.cssSelector("[href$='gin']"));
     }
 
